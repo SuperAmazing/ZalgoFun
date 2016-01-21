3f84e8db6c66cb7cb8fbf8d5ead0295e55e0dd74
Kevin Waldock <kevin.waldock@gmail.com>
2016-01-21 10:39:13 +0100

    Noch ein Commit für Konflikte!

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class MainCls {
     public static void main(String[] args)
     {
         System.out.println("Schöne Grüße von Kevin!")
+        System.out.println("Noch einemal schöne Grüße!")
     }
     
 }
